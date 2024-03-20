b36bccf81067057138b89fc8af711a560f4e97ca
madelyn-h <99363100+madelyn-h@users.noreply.github.com>
2024-03-20 16:12:43 -0500

    test1
    seeing if github works

--- models/dataModel.js
+++ models/dataModel.js
@@ -1,22 +1,40 @@
-const dataModel = {
-  getStoreName: () => {
-    return 'hamsterstore';
-  },
-  getAllListings: () => {
-    return 'all listings';
-  },
-  getProfile: () => {
-    return 'profile';
-  },
-  getProduct: (product_id) => {
-    return 'product id = ' + product_id ;
-  },
-  getCart: () => {
-    return 'cart';
-  },
-  getOrders: () => {
-    return 'orders';
-  },
-};
+const express = require("express");
+const mysql = require("mysql");
+const mysqlConnection = require("../utils/database");
 
+const dataModel = {
+    getAllListings: () => {
+      return 'all listings';
+    },
+    getProfile: (data,callback) => {
+
+	  console.log("running getProfile");
+	  console.log("data is: " + data);
+	  var profile;
+	  
+	  var sql = mysql.format("SELECT * FROM userprofile WHERE username =?",[data]);
+
+	  mysqlConnection.query(sql, (err, results, fields) => {
+      	  	if (err) {
+          	 throw err
+          	} else {
+          	 console.log("results[0]: " + results[0].username);
+	  	 profile = results;
+          	}
+	});
+
+      return profile;
+    },
+    getProduct: (product_id) => {
+      return 'product id = ' + product_id ;
+    },
+    getCart: () => {
+      return 'cart';
+    },
+    getOrders: () => {
+      return 'orders';
+    },
+  };
+  
 module.exports = dataModel;
+//module.exports = Router;
