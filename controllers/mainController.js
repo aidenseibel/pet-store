67ffa96503993aecf1ac39742910da4a110cbcf4
aidenseibel <aiden.seibel@gmail.com>
2024-03-26 12:24:32 -0500

    Added additional logic to mainController

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,6 +4,8 @@ const mainController = {
   getOnboarding: (req, res) => {
     res.render('onboarding');
   },
+
+
   getProfile: (req, res) => {
     const d = 'secondusertest'
     const profile = dataModel.getProfile(d);
@@ -15,29 +17,50 @@ const mainController = {
       res.render('onboarding');
     }
   },
+
+
   getListings: (req, res) => {
     const all_listings = dataModel.getAllListings();
     const profile = dataModel.getProfile();
     res.render('listings', { all_listings, profile });
   },
+
+
   getUserProductView: (req, res) => {
     const product_id = req.body.product_id;
     const product = dataModel.getProduct(product_id);
     res.render('user/user_product_view', { product });
   },
+
+
   getUserCart: (req, res) => {
-    const cart = dataModel.getCart();
+    const profile = dataModel.getProfile();
+    const cart = dataModel.getCart(profile);
     res.render('user/user_cart', { cart });
   },
+
+
   getUserCheckout: (req, res) => {
+    const profile = dataModel.getProfile();
     const cart = dataModel.getCart();
-    res.render('user/user_checkout', { cart });
+    res.render('user/user_checkout', { profile, cart });
   },
+
+
   getVendorDashboard: (req, res) => {
     const profile = dataModel.getProfile();
     const orders = dataModel.getOrders();
-    res.render('vendor/vendor_dashboard', { profile, orders });
+
+    // lil logic check to make sure you're a vendor
+    // if(profile.isVendor){
+      res.render('vendor/vendor_dashboard', { profile, orders });
+    // }
+    // else{
+    //   res.render('vendor/not_vendor', { profile, orders });
+    // }
   },
+
+  
   getVendorProductView: (req, res) => {
     const product_id = req.body.product_id;
     const product = dataModel.getProduct(product_id);
